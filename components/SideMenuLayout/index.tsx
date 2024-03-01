8de759a80cd351dca7a1034421fbef2a7836ff9e
deluxesande <deluxesande@gmail.com>
2024-03-01 15:59:49 +0300

    (Add): top bar for the side menu layout

--- components/SideMenuLayout/index.tsx
+++ components/SideMenuLayout/index.tsx
@@ -7,8 +7,12 @@ import {
     UserCircle,
     Receipt,
     User,
+    Search,
+    Moon,
 } from "lucide-react";
 import { useRouter } from "next/router";
+import { Input } from "../ui/input";
+import Image from "next/image";
 
 const SideMenuLayout: React.FC<{ children: React.ReactNode }> = ({
     children,
@@ -41,15 +45,44 @@ const SideMenuLayout: React.FC<{ children: React.ReactNode }> = ({
                 <SideMenuItem icon={<User size={20} />} text="Profile" />
             </SideMenu>
             <div className="flex-grow p-6 bg-slate-200">
-                <div className="mb-4">
-                    <p className="text-sm font-light">Pages / Dashboard</p>
-                    <h1 className="text-primary font-semibold text-3xl my-1">
-                        Main Dashboard
-                    </h1>
-                    <p className="text-md font-medium text-green-500 mt-3">
-                        Chuka Branch
-                    </p>
+                <div className="flex justify-between">
+                    <div>
+                        <p className="text-sm font-light">Pages / Dashboard</p>
+                        <h1 className="text-primary font-semibold text-3xl my-1">
+                            Main Dashboard
+                        </h1>
+                    </div>
+                    <div className="flex items-center gap-3 bg-slate-100 py-[1px] px-4 rounded-full">
+                        <div className="relative">
+                            <Search
+                                color="#777"
+                                size={15}
+                                className="absolute left-3 top-[5px] transform translate-y-[5px]"
+                            />
+                            <Input
+                                id="search"
+                                className="rounded-full pl-10 w-72"
+                                placeholder="Search"
+                                type="text"
+                            />
+                        </div>
+                        <div className="flex gap-3 items-center">
+                            <Moon size={15} />
+                            <div className="relative w-8 h-8">
+                                <Image
+                                    src="https://randomuser.me/api/portraits/men/80.jpg"
+                                    alt=""
+                                    className="object-cover rounded-full shadow-lg"
+                                    layout="fill"
+                                    objectFit="cover"
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <p className="text-md font-medium text-green-500 mt-3 mb-4">
+                    Chuka Branch
+                </p>
                 {children}
             </div>
         </div>
